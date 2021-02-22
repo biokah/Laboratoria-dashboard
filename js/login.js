39ba178f116165bbe2d1241762b7614c5b730e6d
Anais <anais@photoshelter.com>
2021-02-21 21:43:07 -0600

    removing login validation

--- js/login.js
+++ js/login.js
@@ -6,9 +6,6 @@ function validate(e) {
     e.preventDefault();
     var user = data.querySelector('input[type="text"]')
     var password = data.querySelector('input[type="password"]')
-    if (user.value === 'amalia@laboratoria.la' && password.value === 'amalia@laboratoria.la') {
-        window.location.href = 'dashboard.html'
-    } else {
-        alert('Ingresa una cuenta válida')
-    }
+    window.location.href = 'dashboard.html'
+    
 }
